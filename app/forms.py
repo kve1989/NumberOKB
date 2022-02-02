68e07532387dc2ecb3647edea7418accf0f63e5b
kve1989 <kve1989@ya.ru>
2022-02-02 16:53:52 +0500

    change string field to int

--- app/forms.py
+++ app/forms.py
@@ -1,17 +1,17 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, DateField
+from wtforms import StringField, DateField, IntegerField
 from wtforms.validators import DataRequired
 
 class MyForm(FlaskForm):
     date = DateField('Дата', validators=[DataRequired()])
-    sent = StringField('Отправлено', validators=[DataRequired()])
-    mistakes = StringField('Ошибки', validators=[DataRequired()])
+    sent = IntegerField('Отправлено', validators=[DataRequired()])
+    mistakes = IntegerField('Ошибки', validators=[DataRequired()])
 
 class PCRform(MyForm):
-    done = StringField('Выполнено', validators=[DataRequired()])
+    done = IntegerField('Выполнено', validators=[DataRequired()])
 
 class MCEform(MyForm):
-    assigned  = StringField('Назначено', validators=[DataRequired()])
+    assigned  = IntegerField('Назначено', validators=[DataRequired()])
 
 class SearchForm(FlaskForm):
     date = DateField('Выбрать даты', validators=[DataRequired()])
