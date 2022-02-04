84522decde47f5c4bbdd952d83670c009974e97e
kve1989 <kve1989@ya.ru>
2022-02-04 16:51:36 +0500

    Add commnets

--- app/forms.py
+++ app/forms.py
@@ -3,15 +3,19 @@ from wtforms import StringField, DateField, IntegerField
 from wtforms.validators import DataRequired
 
 class MyForm(FlaskForm):
+    # Определяем форму с полями и берем за основу
     date = DateField('Дата', validators=[DataRequired()])
     sent = IntegerField('Отправлено', validators=[DataRequired()])
     mistakes = IntegerField('Ошибки', validators=[DataRequired()])
 
 class PCRform(MyForm):
+    # Форма для ПЦР
     done = IntegerField('Выполнено', validators=[DataRequired()])
 
 class MCEform(MyForm):
+    # Форма для МСЭ
     assigned  = IntegerField('Назначено', validators=[DataRequired()])
 
 class SearchForm(FlaskForm):
+    # Форма для поиска
     date = DateField('Выбрать даты', validators=[DataRequired()])
