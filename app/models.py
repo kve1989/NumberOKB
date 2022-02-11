f6c80dd9bdab5fe69f9366060bd5c6058267674c
kve1989 <kve1989@ya.ru>
2022-02-11 15:58:53 +0500

    fix models

--- app/models.py
+++ app/models.py
@@ -6,9 +6,9 @@ class ProtocConsult(db.Model):
     __tablename__ = '1.ProtocConsult'
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.Date)
-    done = db.Column(db.Integer)
-    sent = db.Column(db.Integer)
-    mistakes = db.Column(db.Integer)
+    done = db.Column(db.Integer, nullable=True, default=0)
+    sent = db.Column(db.Integer, nullable=True, default=0)
+    mistakes = db.Column(db.Integer, nullable=True, default=0)
 
     def __repr__(self):
         return '<ProtocConsult %r>' % self.id
@@ -19,9 +19,9 @@ class ProfMedicalExam(db.Model):
     __tablename__ = '2.ProfMedicalExam'
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.Date)
-    done = db.Column(db.Integer)
-    sent = db.Column(db.Integer)
-    mistakes = db.Column(db.Integer)
+    done = db.Column(db.Integer, nullable=True, default=0)
+    sent = db.Column(db.Integer, nullable=True, default=0)
+    mistakes = db.Column(db.Integer, nullable=True, default=0)
 
     def __repr__(self):
         return '<ProfMedicalExam %r>' % self.id
@@ -32,9 +32,9 @@ class LabResearch(db.Model):
     __tablename__ = '3.LabResearch'
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.Date)
-    done = db.Column(db.Integer)
-    sent = db.Column(db.Integer)
-    mistakes = db.Column(db.Integer)
+    done = db.Column(db.Integer, nullable=True, default=0)
+    sent = db.Column(db.Integer, nullable=True, default=0)
+    mistakes = db.Column(db.Integer, nullable=True, default=0)
 
     def __repr__(self):
         return '<LabResearch %r>' % self.id
@@ -45,9 +45,9 @@ class MedCertBirth(db.Model):
     __tablename__ = '4.MedCertBirth'
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.Date)
-    done = db.Column(db.Integer)
-    sent = db.Column(db.Integer)
-    mistakes = db.Column(db.Integer)
+    done = db.Column(db.Integer, nullable=True, default=0)
+    sent = db.Column(db.Integer, nullable=True, default=0)
+    mistakes = db.Column(db.Integer, nullable=True, default=0)
 
     def __repr__(self):
         return '<MedCertBirth %r>' % self.id
@@ -58,9 +58,9 @@ class DirectionOnMCE(db.Model):
     __tablename__ = '5.DirectionOnMCE'
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.Date)
-    done = db.Column(db.Integer)
-    sent = db.Column(db.Integer)
-    mistakes = db.Column(db.Integer)
+    done = db.Column(db.Integer, nullable=True, default=0)
+    sent = db.Column(db.Integer, nullable=True, default=0)
+    mistakes = db.Column(db.Integer, nullable=True, default=0)
 
     def __repr__(self):
         return '<DirectionOnMCE %r>' % self.id
@@ -71,9 +71,9 @@ class DirectToHospRehabExamCons(db.Model):
     __tablename__ = '6.DirectToHospRehabExamCons'
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.Date)
-    done = db.Column(db.Integer)
-    sent = db.Column(db.Integer)
-    mistakes = db.Column(db.Integer)
+    done = db.Column(db.Integer, nullable=True, default=0)
+    sent = db.Column(db.Integer, nullable=True, default=0)
+    mistakes = db.Column(db.Integer, nullable=True, default=0)
 
     def __repr__(self):
         return '<DirectToHospRehabExamCons %r>' % self.id
@@ -84,9 +84,9 @@ class ProtocInstrumentStudies(db.Model):
     __tablename__ = '7.ProtocInstrumentStudies'
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.Date)
-    done = db.Column(db.Integer)
-    sent = db.Column(db.Integer)
-    mistakes = db.Column(db.Integer)
+    done = db.Column(db.Integer, nullable=True, default=0)
+    sent = db.Column(db.Integer, nullable=True, default=0)
+    mistakes = db.Column(db.Integer, nullable=True, default=0)
 
     def __repr__(self):
         return '<ProtocInstrumentStudies %r>' % self.id
@@ -97,9 +97,9 @@ class MilkMixture(db.Model):
     __tablename__ = '8.MilkMixture'
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.Date)
-    done = db.Column(db.Integer)
-    sent = db.Column(db.Integer)
-    mistakes = db.Column(db.Integer)
+    done = db.Column(db.Integer, nullable=True, default=0)
+    sent = db.Column(db.Integer, nullable=True, default=0)
+    mistakes = db.Column(db.Integer, nullable=True, default=0)
 
     def __repr__(self):
         return '<MilkMixture %r>' % self.id
@@ -110,9 +110,9 @@ class Recipes(db.Model):
     __tablename__ = '9.Recipes'
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.Date)
-    done = db.Column(db.Integer)
-    sent = db.Column(db.Integer)
-    mistakes = db.Column(db.Integer)
+    done = db.Column(db.Integer, nullable=True, default=0)
+    sent = db.Column(db.Integer, nullable=True, default=0)
+    mistakes = db.Column(db.Integer, nullable=True, default=0)
 
     def __repr__(self):
         return '<Recipes %r>' % self.id
@@ -123,9 +123,9 @@ class Statcard(db.Model):
     __tablename__ = '10.Statcard'
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.Date)
-    done = db.Column(db.Integer)
-    sent = db.Column(db.Integer)
-    mistakes = db.Column(db.Integer)
+    done = db.Column(db.Integer, nullable=True, default=0)
+    sent = db.Column(db.Integer, nullable=True, default=0)
+    mistakes = db.Column(db.Integer, nullable=True, default=0)
 
     def __repr__(self):
         return '<Statcard %r>' % self.id
@@ -136,9 +136,9 @@ class EmergencyNotice(db.Model):
     __tablename__ = '11.EmergencyNotice'
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.Date)
-    done = db.Column(db.Integer)
-    sent = db.Column(db.Integer)
-    mistakes = db.Column(db.Integer)
+    done = db.Column(db.Integer, nullable=True, default=0)
+    sent = db.Column(db.Integer, nullable=True, default=0)
+    mistakes = db.Column(db.Integer, nullable=True, default=0)
 
     def __repr__(self):
         return '<EmergencyNotice %r>' % self.id
@@ -149,9 +149,9 @@ class EpicrisisExtrFromPatientsMedRecs(db.Model):
     __tablename__ = '12.EpicrisisExtrFromPatientsMedRecs'
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.Date)
-    done = db.Column(db.Integer)
-    sent = db.Column(db.Integer)
-    mistakes = db.Column(db.Integer)
+    done = db.Column(db.Integer, nullable=True, default=0)
+    sent = db.Column(db.Integer, nullable=True, default=0)
+    mistakes = db.Column(db.Integer, nullable=True, default=0)
 
     def __repr__(self):
         return '<EpicrisisExtrFromPatientsMedRecs %r>' % self.id
@@ -162,9 +162,9 @@ class EpicrisisDaccounting(db.Model):
     __tablename__ = '13.EpicrisisDaccounting'
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.Date)
-    done = db.Column(db.Integer)
-    sent = db.Column(db.Integer)
-    mistakes = db.Column(db.Integer)
+    done = db.Column(db.Integer, nullable=True, default=0)
+    sent = db.Column(db.Integer, nullable=True, default=0)
+    mistakes = db.Column(db.Integer, nullable=True, default=0)
 
     def __repr__(self):
         return '<EpicrisisDaccounting %r>' % self.id
@@ -175,9 +175,9 @@ class EpicrisisDischarge(db.Model):
     __tablename__ = '14.EpicrisisDischarge'
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.Date)
-    done = db.Column(db.Integer)
-    sent = db.Column(db.Integer)
-    mistakes = db.Column(db.Integer)
+    done = db.Column(db.Integer, nullable=True, default=0)
+    sent = db.Column(db.Integer, nullable=True, default=0)
+    mistakes = db.Column(db.Integer, nullable=True, default=0)
 
     def __repr__(self):
         return '<EpicrisisDischarge %r>' % self.id
@@ -188,9 +188,9 @@ class EpicrisisTranslation(db.Model):
     __tablename__ = '15.EpicrisisTranslation'
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.Date)
-    done = db.Column(db.Integer)
-    sent = db.Column(db.Integer)
-    mistakes = db.Column(db.Integer)
+    done = db.Column(db.Integer, nullable=True, default=0)
+    sent = db.Column(db.Integer, nullable=True, default=0)
+    mistakes = db.Column(db.Integer, nullable=True, default=0)
 
     def __repr__(self):
         return '<EpicrisisTranslation %r>' % self.id
@@ -201,9 +201,9 @@ class EpicrisisPosthumous(db.Model):
     __tablename__ = '16.EpicrisisPosthumous'
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.Date)
-    done = db.Column(db.Integer)
-    sent = db.Column(db.Integer)
-    mistakes = db.Column(db.Integer)
+    done = db.Column(db.Integer, nullable=True, default=0)
+    sent = db.Column(db.Integer, nullable=True, default=0)
+    mistakes = db.Column(db.Integer, nullable=True, default=0)
 
     def __repr__(self):
         return '<EpicrisisPosthumous %r>' % self.id
@@ -214,9 +214,9 @@ class EpicrisisPreoperative(db.Model):
     __tablename__ = '17.EpicrisisPreoperative'
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.Date)
-    done = db.Column(db.Integer)
-    sent = db.Column(db.Integer)
-    mistakes = db.Column(db.Integer)
+    done = db.Column(db.Integer, nullable=True, default=0)
+    sent = db.Column(db.Integer, nullable=True, default=0)
+    mistakes = db.Column(db.Integer, nullable=True, default=0)
 
     def __repr__(self):
         return '<EpicrisisPreoperative %r>' % self.id
@@ -227,9 +227,9 @@ class EpicrisisDremoval(db.Model):
     __tablename__ = '18.EpicrisisDremoval'
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.Date)
-    done = db.Column(db.Integer)
-    sent = db.Column(db.Integer)
-    mistakes = db.Column(db.Integer)
+    done = db.Column(db.Integer, nullable=True, default=0)
+    sent = db.Column(db.Integer, nullable=True, default=0)
+    mistakes = db.Column(db.Integer, nullable=True, default=0)
 
     def __repr__(self):
         return '<EpicrisisDremoval %r>' % self.id
@@ -240,9 +240,9 @@ class EpicrisisStage(db.Model):
     __tablename__ = '19.EpicrisisStage'
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.Date)
-    done = db.Column(db.Integer)
-    sent = db.Column(db.Integer)
-    mistakes = db.Column(db.Integer)
+    done = db.Column(db.Integer, nullable=True, default=0)
+    sent = db.Column(db.Integer, nullable=True, default=0)
+    mistakes = db.Column(db.Integer, nullable=True, default=0)
 
     def __repr__(self):
         return '<EpicrisisStage %r>' % self.id
@@ -253,9 +253,9 @@ class VaccinationAll(db.Model):
     __tablename__ = '20.Vaccination(all)'
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.Date)
-    done = db.Column(db.Integer)
-    sent = db.Column(db.Integer)
-    mistakes = db.Column(db.Integer)
+    done = db.Column(db.Integer, nullable=True, default=0)
+    sent = db.Column(db.Integer, nullable=True, default=0)
+    mistakes = db.Column(db.Integer, nullable=True, default=0)
 
     def __repr__(self):
         return '<VaccinationAll %r>' % self.id
@@ -265,9 +265,9 @@ class VaccinationCovid(db.Model):
     __tablename__ = '21.Vaccination(Covid)'
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.Date)
-    done = db.Column(db.Integer)
-    sent = db.Column(db.Integer)
-    mistakes = db.Column(db.Integer)
+    done = db.Column(db.Integer, nullable=True, default=0)
+    sent = db.Column(db.Integer, nullable=True, default=0)
+    mistakes = db.Column(db.Integer, nullable=True, default=0)
 
     def __repr__(self):
         return '<VaccinationCovid %r>' % self.id
@@ -277,9 +277,9 @@ class CallDocAtHome(db.Model):
     __tablename__ = '22.CallDocAtHome'
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.Date)
-    done = db.Column(db.Integer)
-    sent = db.Column(db.Integer)
-    mistakes = db.Column(db.Integer)
+    done = db.Column(db.Integer, nullable=True, default=0)
+    sent = db.Column(db.Integer, nullable=True, default=0)
+    mistakes = db.Column(db.Integer, nullable=True, default=0)
 
     def __repr__(self):
         return '<CallDocAtHome %r>' % self.id
@@ -290,9 +290,9 @@ class DeathInform(db.Model):
     __tablename__ = '23.DeathInform'
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.Date)
-    done = db.Column(db.Integer)
-    sent = db.Column(db.Integer)
-    mistakes = db.Column(db.Integer)
+    done = db.Column(db.Integer, nullable=True, default=0)
+    sent = db.Column(db.Integer, nullable=True, default=0)
+    mistakes = db.Column(db.Integer, nullable=True, default=0)
 
     def __repr__(self):
         return '<DeathInform %r>' % self.id
@@ -303,9 +303,9 @@ class InformPerinatalDeath(db.Model):
     __tablename__ = '24.InformPerinatalDeath'
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.Date)
-    done = db.Column(db.Integer)
-    sent = db.Column(db.Integer)
-    mistakes = db.Column(db.Integer)
+    done = db.Column(db.Integer, nullable=True, default=0)
+    sent = db.Column(db.Integer, nullable=True, default=0)
+    mistakes = db.Column(db.Integer, nullable=True, default=0)
 
     def __repr__(self):
         return '<InformPerinatalDeath %r>' % self.id
@@ -316,9 +316,9 @@ class ELNOpen(db.Model):
     __tablename__ = '25.ELN(open)'
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.Date)
-    done = db.Column(db.Integer)
-    sent = db.Column(db.Integer)
-    mistakes = db.Column(db.Integer)
+    done = db.Column(db.Integer, nullable=True, default=0)
+    sent = db.Column(db.Integer, nullable=True, default=0)
+    mistakes = db.Column(db.Integer, nullable=True, default=0)
 
     def __repr__(self):
         return '<ELNOpen %r>' % self.id
@@ -329,9 +329,9 @@ class ELNClose(db.Model):
     __tablename__ = '26.ELN(close)'
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.Date)
-    done = db.Column(db.Integer)
-    sent = db.Column(db.Integer)
-    mistakes = db.Column(db.Integer)
+    done = db.Column(db.Integer, nullable=True, default=0)
+    sent = db.Column(db.Integer, nullable=True, default=0)
+    mistakes = db.Column(db.Integer, nullable=True, default=0)
 
     def __repr__(self):
         return '<ELNClose %r>' % self.id
