b5e349285dcc27f66461f3708a9a2186d25fb26c
kve1989 <kve1989@ya.ru>
2023-01-24 11:23:15 +0500

    fix signs

--- app/sign.py
+++ app/sign.py
@@ -1,9 +1,17 @@
-from flask import render_template, request, redirect, flash, session, url_for
+from flask import render_template, request, redirect, flash, session, url_for, send_from_directory
 from app import app, db
 from app.models import Signs
 from app.forms import SignForm
 from datetime import datetime
 from werkzeug.utils import secure_filename
+import hashlib
+import os
+
+def generateFilename(filename):
+    f_name = filename.rsplit('.', 1)
+    f_name[0] = hashlib.md5(f_name[0].encode('utf-8')).hexdigest()
+    return '.'.join(f_name)
+
 
 @app.route('/sign')
 def page_sign_index():
@@ -17,6 +25,7 @@ def page_sign_create():
 
 @app.route('/sign/<int:id>/edit')
 def page_sign_edit(id):
+    form = SignForm()
     pass
 
 @app.route('/sign/store', methods=['POST'])
@@ -27,8 +36,8 @@ def sign_store():
     dateEnd = datetime.strptime(request.form['dateEnd'], "%Y-%m-%d")
     owner = request.form['owner']
     typeCertificate = request.form['typeCertificate']
-    fileCertificate = secure_filename(form.fileCertificate.data.filename)
-    fileContainer = secure_filename(form.fileContainer.data.filename)
+    fileCertificate = generateFilename( secure_filename(form.fileCertificate.data.filename) )
+    fileContainer = generateFilename( secure_filename(form.fileContainer.data.filename) )
 
     if form.validate_on_submit():
         record = Signs(
@@ -43,6 +52,8 @@ def sign_store():
         try:
             db.session.add(record)
             db.session.commit()
+            request.files['fileCertificate'].save(os.path.join(app.config['UPLOAD_FOLDER'], fileCertificate))
+            request.files['fileContainer'].save(os.path.join(app.config['UPLOAD_FOLDER'], fileContainer))
             flash("Запись успешно добавлена!", 'success')
         except:
             flash("Ошибка при добавлении!", 'danger')
@@ -57,6 +68,7 @@ def sign_store():
 
 @app.route('/sign/<int:id>/update')
 def sign_update(id):
+    record = Signs.query.get_or_404(id)
     pass
 
 @app.route('/sign/<int:id>/delete')
@@ -66,8 +78,18 @@ def sign_delete(id):
     try:
         db.session.delete(record)
         db.session.commit()
+        if os.path.exists(os.path.join(app.config['UPLOAD_FOLDER'], record.fileCertificate)):
+            os.remove(os.path.join(app.config['UPLOAD_FOLDER'], record.fileCertificate))
+
+        if os.path.exists(os.path.join(app.config['UPLOAD_FOLDER'], record.fileContainer)):
+            os.remove(os.path.join(app.config['UPLOAD_FOLDER'], record.fileContainer))
         flash("Запись успешно удалена!", 'success')
     except:
         flash("Ошибка при удалении!", 'danger')
 
     return redirect(url_for('page_sign_index'))
+
+
+@app.route('/uploads/<file>')
+def download_file(file):
+    return send_from_directory(app.config["UPLOAD_FOLDER"], file)
