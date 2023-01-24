74b7f9823d68ed61fcc5d49e77325e564e8fac61
kve1989 <kve1989@ya.ru>
2023-01-24 11:21:15 +0500

    fix Config

--- config.py
+++ config.py
@@ -1,9 +1,8 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-
 class Config:
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'db.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = '01f260718007c0bd2ef3e9005b84ab97'
-    UPLOAD_FOLDER = '/uploads'
+    UPLOAD_FOLDER = os.path.join(basedir, 'app/uploads')
