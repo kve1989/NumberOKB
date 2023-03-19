0f6caa9677f089f45587d554380c53ebca9905cd
kve1989 <kve1989@ya.ru>
2023-03-19 20:41:51 +0500

    add init uploads folder

--- run.py
+++ run.py
@@ -1,4 +1,9 @@
 from app import app
+import os
+
+"""Проверяем на наличие папки для загрузок"""
+if (not(os.path.exists(app.config['UPLOAD_FOLDER']))):
+    os.mkdir(app.config['UPLOAD_FOLDER'])
 
 if __name__ == '__main__':
     app.run(debug=True)
